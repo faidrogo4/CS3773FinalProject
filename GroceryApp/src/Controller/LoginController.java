7bf69fd473f93d821ac208a23372489275089e7b
irishan123 <43056673+irishan123@users.noreply.github.com>
2021-05-02 00:07:11 -0500

    Modified Signup fxml and controller, Login controller; added more icon images to image folder

--- GroceryApp/src/Controller/LoginController.java
+++ GroceryApp/src/Controller/LoginController.java
@@ -1,5 +1,23 @@
 package Controller;
 
-public class LoginController {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 
+import javafx.fxml.FXML;
+
+public class LoginController {
+	
+	@FXML
+	public JFXButton LoginButton;
+	
+	@FXML
+	public JFXButton BackButton;
+	
+	@FXML
+	public JFXTextField EmailField;
+	
+	@FXML
+	public JFXPasswordField PasswordField;
+	
 }
--- GroceryApp/src/Controller/SignupController.java
+++ GroceryApp/src/Controller/SignupController.java
@@ -1,5 +1,33 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXPasswordField;
+
 public class SignupController {
 
+	@FXML
+	public JFXButton BackButton;
+	
+	@FXML
+	public JFXButton RegisterButton;
+	
+	@FXML
+	public JFXTextField FirstName;
+	
+	@FXML
+	public JFXTextField LastName;
+	
+	@FXML
+	public JFXTextField EmailField;
+	
+	@FXML
+	public JFXPasswordField PasswordField;
+	
+	
+	
+	
+	
+	
 }
