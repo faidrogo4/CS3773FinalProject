ddd6b16782d4757da418b9ea957d5a70932b7e01
irishan123 <43056673+irishan123@users.noreply.github.com>
2021-05-01 20:33:15 -0500

    Created Initial fxml and controller, added jfoenix jar to classpath, added images, cleaned up packages

---
+++ GroceryApp/src/Controller/InitialController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import com.jfoenix.controls.JFXButton;
+
+public class InitialController {
+	
+	@FXML
+	public JFXButton LoginButton;
+	
+	@FXML
+	public JFXButton SignupButton;
+	
+}
