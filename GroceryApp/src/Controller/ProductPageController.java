11b7e8803445e945bf07e9ee2920ecfa149515a9
Frank Idrogo <f.a.idrogo@gmail.com>
2021-05-06 20:14:01 -0500

    Item.fxml references AddItemToCartButton on click. ItemController - added AddItemToCartButton and order. LoadFxml - loads views ProductPageController - add views into gridpane PPView - udpated the gridpane to fit views ProductTest - junit test for Product (not finished, still adding to it)

--- GroceryApp/src/Controller/ProductPageController.java
+++ GroceryApp/src/Controller/ProductPageController.java
@@ -3,21 +3,27 @@ package Controller;
 import application.Main;
 import com.jfoenix.controls.JFXButton;
 
+import Model.LoadFxml;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class ProductPageController {
+public class ProductPageController implements Initializable{
 
 	@FXML
 	public JFXButton HomeButton;
@@ -32,10 +38,49 @@ public class ProductPageController {
 	public JFXButton CartButton;
 
 	public ScrollPane productViewer;
-
+	
 	@FXML
-	public void initialize() {
-	}
+	public GridPane grid;
+
+	
+	/*
+	 * initialize() will load products into grid on ProductPageView load.
+	 * */
+	
+	@Override
+    public void initialize(URL location, ResourceBundle resources) {
+		//grid.getRowConstraints().add(new RowConstraints(0));
+		//grid.getColumnConstraints().add(new ColumnConstraints(0));
+		grid.setGridLinesVisible(true);
+		LoadFxml object = new LoadFxml();
+		Pane view = object.getPage("Item");
+		Pane view1 = object.getPage("Item");
+		Pane view2 = object.getPage("Item");
+		Pane view3 = object.getPage("Item");
+		Pane view4 = object.getPage("Item");
+		Pane view5 = object.getPage("Item");
+		Pane view6 = object.getPage("Item");
+		Pane view7 = object.getPage("Item");
+
+		if(view == null
+		  || view1 == null
+		  || view2 == null
+		  || view3 == null
+		  || view4 == null
+		  || view5 == null
+		  || view6 == null
+		  || view7 == null) { System.out.println("A view is null"); }
+		
+		grid.add(view, 0, 0, 1, 1);
+		grid.add(view1, 1, 0, 1, 1);
+		grid.add(view2, 0, 1, 1, 1);
+		grid.add(view3, 1, 1, 1, 1);
+		grid.add(view4, 0, 2, 1, 1);
+		grid.add(view5, 1, 2, 1, 1);
+		grid.add(view6, 0, 3, 1, 1);
+		grid.add(view7, 1, 3, 1, 1);
+		
+    }
 
 
 	public void homePress(final ActionEvent actionEvent) throws IOException{
