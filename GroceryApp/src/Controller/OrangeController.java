e90f8dfa1f42f9114352b3ccffc235e604d4f351
irishan123 <43056673+irishan123@users.noreply.github.com>
2021-05-07 19:00:36 -0500

    Created views and controllers for each of the products; fixed typo on home page

---
+++ GroceryApp/src/Controller/OrangeController.java
@@ -0,0 +1,55 @@
+package Controller;
+
+import Model.Order;
+import Model.Product;
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+
+public class OrangeController {
+
+	@FXML
+	private Label nameLabel;
+	
+	@FXML 
+	private Label priceLabel;
+	
+	@FXML
+	private ImageView itemImage;
+	
+	@FXML
+	public JFXButton addToCartButton;
+	
+	private Product item;
+	private Order currOrder;
+	public void initialize(){
+		currOrder = Main.current.getOrder();
+	}
+	public void setData(Product item) {
+		this.item = item;
+		nameLabel.setText(item.getName());
+		priceLabel.setText("$" + item.getPrice());
+		//Image image = new Image(getClass().getResourceAsStream(item.GetPImgSrc()));
+		//itemImage.setImage(image);
+	}
+	
+	public void addToOrder(Order currOrder, Product item) {
+		this.currOrder = currOrder;		
+		currOrder.addProduct(item);
+	}
+	
+	@FXML
+	void handleAddToCartButton(ActionEvent event) throws IOException {
+		addToOrder(currOrder, item);
+		System.out.println("Item clicked.");
+		System.out.println(item + "added to order");
+	}
+	
+}
